a29170be84066fa363ee95e920d4abf8b04cb56f
Marlon <martianworm17@gmail.com>
2017-11-01 00:58:58 +0800

    Hello world in C. Initial Commit.

---
+++ tp_c/hello.c
@@ -0,0 +1,17 @@
+/*
+ * hello.c
+ *
+ *  Created on: 1 Nov 2017
+ *      Author: Lon
+ */
+#include <stdio.h>
+
+int main() {
+	/* my first program in C */
+	printf("Hello, World! \n");
+
+	return 0;
+}
+
+
+
